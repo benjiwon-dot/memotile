5a26acf63fc7174705b4c7e3e3dec046c199cc0c
Benjamin <ben.jiwon@kangkook.com>
2026-06-13 20:50:56 +0700

    fix: 복합 에러 분석 완료. Xcode 16 환경에서 fmt 모듈만 C++17로 핀셋 우회

--- withFmtFix.js
+++ withFmtFix.js
@@ -9,23 +9,24 @@ module.exports = function withFmtFix(config) {
       const podfile = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       if (fs.existsSync(podfile)) {
         let contents = fs.readFileSync(podfile, 'utf-8');
-        const fixCode = `
+        const fix = `
   installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] ||= ['$(inherited)']
-      config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_HAS_CONSTEVAL=0'
+    if target.name == 'fmt'
+      target.build_configurations.each do |config|
+        config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
+      end
     end
   end
 `;
-        if (!contents.includes('FMT_HAS_CONSTEVAL=0')) {
+        if (!contents.includes("target.name == 'fmt'")) {
           contents = contents.replace(
             /post_install do \|installer\|/g,
-            `post_install do |installer|\n${fixCode}`
+            `post_install do |installer|\n${fix}`
           );
           fs.writeFileSync(podfile, contents);
         }
       }
       return config;
-    },
+    }
   ]);
 };
