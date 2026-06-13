5b547d711dab22c42dd2d09947c6942dc987ce10
Benjamin <ben.jiwon@kangkook.com>
2026-06-13 15:35:34 +0700

    fix: fmt 라이브러리 C++17 강제 다운그레이드 플러그인 적용

---
+++ withFmtFix.js
@@ -0,0 +1,28 @@
+const { withDangerousMod } = require('@expo/config-plugins');
+const fs = require('fs');
+const path = require('path');
+
+module.exports = function withFmtFix(config) {
+  return withDangerousMod(config, [
+    'ios',
+    async (config) => {
+      const podfile = path.join(config.modRequest.platformProjectRoot, 'Podfile');
+      if (fs.existsSync(podfile)) {
+        let contents = fs.readFileSync(podfile, 'utf-8');
+        const fix = `post_install do |installer|
+  installer.pods_project.targets.each do |target|
+    if target.name == 'fmt'
+      target.build_configurations.each do |config|
+        config.build_settings['CLANG_CXX_LANGUAGE_STANDARD'] = 'c++17'
+      end
+    end
+  end`;
+        if (!contents.includes("target.name == 'fmt'")) {
+          contents = contents.replace(/post_install do \|installer\|/g, fix);
+          fs.writeFileSync(podfile, contents);
+        }
+      }
+      return config;
+    }
+  ]);
+};
