8c76cb73618d3018e0ceb098e90d34fda7fbd2e7
Benjamin <ben.jiwon@kangkook.com>
2026-06-13 23:21:37 +0700

    fix: Pod install 완료 시점에 물리적 소스 치환 플러그인 탑재로 consteval 에러 영구 박살

--- withFmtFix.js
+++ withFmtFix.js
@@ -5,28 +5,18 @@ const path = require('path');
 module.exports = function withFmtFix(config) {
   return withDangerousMod(config, [
     'ios',
-    async (config) => {
+    (config) => {
       const podfile = path.join(config.modRequest.platformProjectRoot, 'Podfile');
       if (fs.existsSync(podfile)) {
         let contents = fs.readFileSync(podfile, 'utf-8');
-        const fixCode = `
-  installer.pods_project.targets.each do |target|
-    target.build_configurations.each do |config|
-      defs = config.build_settings['GCC_PREPROCESSOR_DEFINITIONS']
-      if defs.nil?
-        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] = ['$(inherited)', 'FMT_HAS_CONSTEVAL=0']
-      elsif defs.is_a?(Array)
-        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] << 'FMT_HAS_CONSTEVAL=0' unless defs.include?('FMT_HAS_CONSTEVAL=0')
-      elsif defs.is_a?(String)
-        config.build_settings['GCC_PREPROCESSOR_DEFINITIONS'] = [defs, 'FMT_HAS_CONSTEVAL=0'] unless defs.include?('FMT_HAS_CONSTEVAL=0')
-      end
-    end
-  end
-`;
-        if (!contents.includes('FMT_HAS_CONSTEVAL=0')) {
+        
+        // 띄어쓰기 돌발 변수까지 완벽 대응하는 최종 정제된 sed 수술 명령어
+        const sedCommand = `  system("find Pods/fmt -type f -name '*.h' -exec sed -i '' -E 's/FMT_CONSTEVAL[[:space:]]+consteval/FMT_CONSTEVAL constexpr/g' {} +")`;
+        
+        if (!contents.includes('find Pods/fmt')) {
           contents = contents.replace(
             /post_install do \|installer\|/g,
-            `post_install do |installer|\n${fixCode}`
+            `post_install do |installer|\n${sedCommand}`
           );
           fs.writeFileSync(podfile, contents);
         }
